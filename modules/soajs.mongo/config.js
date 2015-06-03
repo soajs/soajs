731eb3a1253bdcbd998c4d5cf30a309dec6c1f9c
mikehajj <mike.hajj@gmail.com>
2015-06-03 16:31:39 +0300

    updated mongo driver added support for versioning.

--- modules/soajs.mongo/config.js
+++ modules/soajs.mongo/config.js
@@ -3,6 +3,9 @@
 module.exports = {
     'errors': {
         '190': 'Unable to build needed url for mongo to connect.',
-        '191': 'collection name is required.'
+        '191': 'collection name is required.',
+        "192": "Invalid record provided to Mongo",
+        "193": "Unable to find the record to version it",
+        "194": "Invalid record version provided to Mongo"
     }
 };
