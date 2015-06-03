96a177915874188dc877e4df551969bf4dd54f9e
mikehajj <mike.hajj@gmail.com>
2015-06-03 18:07:34 +0300

    SOAJ-12: added test cases

--- test/unit/soajs.mongo.test.js
+++ test/unit/soajs.mongo.test.js
@@ -46,56 +46,56 @@ describe("testing connection", function() {
 		mongo.find('myCollection', {}, function(error, response) {
 			assert.ok(error);
 			assert.ok(!response);
-            assert.ok(error.message);
+			assert.ok(error.message);
 
 			mongo.findOne('myCollection', {}, function(error, response) {
 				assert.ok(error);
 				assert.ok(!response);
-                assert.ok(error.message);
+				assert.ok(error.message);
 				mongo.insert('myCollection', {}, function(error, response) {
 					assert.ok(error);
 					assert.ok(!response);
-                    assert.ok(error.message);
+					assert.ok(error.message);
 					mongo.save('myCollection', {}, function(error, response) {
 						assert.ok(error);
 						assert.ok(!response);
-                        assert.ok(error.message);
+						assert.ok(error.message);
 						mongo.update('myCollection', {'a': 'b'}, {$set: {'a': 'c'}}, function(error, response) {
 							assert.ok(error);
 							assert.ok(!response);
-                            assert.ok(error.message);
+							assert.ok(error.message);
 							mongo.count('myCollection', {'a': 'b'}, function(error, response) {
 								assert.ok(error);
 								assert.ok(!response);
-                                assert.ok(error.message);
+								assert.ok(error.message);
 								mongo.ensureIndex('myCollection', {'a': 1}, null, function(error, response) {
 									assert.ok(error);
 									assert.ok(!response);
-                                    assert.ok(error.message);
+									assert.ok(error.message);
 									mongo.getCollection('myCollection', function(error, response) {
 										assert.ok(error);
 										assert.ok(!response);
-                                        assert.ok(error.message);
+										assert.ok(error.message);
 										mongo.remove('myCollection', {}, function(error, response) {
 											assert.ok(error);
 											assert.ok(!response);
-                                            assert.ok(error.message);
+											assert.ok(error.message);
 											mongo.findAndModify('myCollection', {'a': 'b'}, {a: 1}, {'a': 'c'}, function(error, response) {
 												assert.ok(error);
 												assert.ok(!response);
-                                                assert.ok(error.message);
+												assert.ok(error.message);
 												mongo.findAndRemove('myCollection', {'a': 'b'}, {a: 1}, function(error, response) {
 													assert.ok(error);
 													assert.ok(!response);
-                                                    assert.ok(error.message);
+													assert.ok(error.message);
 													mongo.dropCollection('myCollection', function(error, response) {
 														assert.ok(error);
 														assert.ok(!response);
-                                                        assert.ok(error.message);
+														assert.ok(error.message);
 														mongo.dropDatabase(function(error, response) {
 															assert.ok(error);
 															assert.ok(!response);
-                                                            assert.ok(error.message);
+															assert.ok(error.message);
 															done();
 														});
 													});
@@ -112,7 +112,7 @@ describe("testing connection", function() {
 		});
 	});
 
-	it("testing with no db name", function(done){
+	it("testing with no db name", function(done) {
 		var dbConfig = {
 			"name": '',
 			"prefix": "soajs_test_",
@@ -146,7 +146,7 @@ describe("testing connection", function() {
 		};
 
 		mongo = new soajsMongo(dbConfig);
-		mongo.find('myCollection',{},function(error){
+		mongo.find('myCollection', {}, function(error) {
 			assert.ok(error);
 			assert.ok(error.message);
 			//assert.equal(error.message,'Unable to build needed url for mongo.connect.');
@@ -481,4 +481,156 @@ describe("TESTING soajs.mongo", function() {
 		});
 	});
 
+});
+
+describe("TESTING soajs.mongo versioning", function() {
+	var mongo = null;
+	before(function(done) {
+		var dbConfig = {
+			"name": 'soajs_test_db',
+			"prefix": "soajs_test_",
+			"servers": [
+				{
+					"host": "127.0.0.1",
+					"port": "27017"
+				}
+			],
+			"credentials": null,
+			"URLParam": {
+				"connectTimeoutMS": 0,
+				"socketTimeoutMS": 0,
+				"maxPoolSize": 5,
+				"w": 1,
+				"wtimeoutMS": 0,
+				"slaveOk": true
+			},
+			"extraParam": {
+				"db": {
+					"native_parser": true
+				},
+				"server": {
+					"auto_reconnect": true
+				}
+			},
+			'store': {},
+			"collection": "sessions",
+			'stringify': false,
+			'expireAfter': 1000 * 60 * 60 * 24 * 14 // 2 weeks
+		};
+		mongo = new soajsMongo(dbConfig);
+
+		mongo.dropDatabase(function(error) {
+			assert.ifError(error);
+			done();
+		});
+	});
+
+	after(function(done) {
+		mongo.dropDatabase(function(error) {
+			assert.ifError(error);
+			done();
+		});
+	});
+
+	it("insert one record", function(done) {
+		mongo.insert("myCollection", {'a': 'b'}, true, function(error, response) {
+			assert.ifError(error);
+			assert.ok(response);
+			mongo.findOne('myCollection', {'a': "b"}, function(error, record) {
+				assert.ifError(error);
+				assert.ok(record);
+				assert.equal(record.v, 1);
+				assert.ok(record.ts);
+				done();
+			});
+		});
+	});
+
+	it("save one record", function(done) {
+		mongo.findOne("myCollection", {'a': 'b'}, function(error, oneRecord) {
+			assert.ifError(error);
+			assert.ok(oneRecord);
+
+			mongo.save("myCollection", oneRecord, true, function(error, response) {
+				assert.ifError(error);
+				assert.ok(response);
+
+				mongo.findOne('myCollection', {'a': "b"}, function(error, record) {
+					assert.ifError(error);
+					assert.ok(record);
+					assert.equal(record.v, 2);
+					assert.ok(record.ts);
+					done();
+				});
+			});
+		});
+	});
+
+	it("update one record", function(done) {
+		mongo.update("myCollection", {'a': 'b'}, {$set: {'a': 'c'}}, true, function(error, response) {
+			assert.ifError(error);
+			assert.ok(response);
+			mongo.findOne('myCollection', {'a': "c"}, function(error, record) {
+				assert.ifError(error);
+				assert.ok(record);
+				assert.equal(record.v, 3);
+				assert.ok(record.ts);
+				done();
+			});
+		});
+	});
+
+	it("get one record version", function(done) {
+		mongo.findOne('myCollection', {'a': 'c'}, function(error, oneRecord) {
+			assert.ifError(error);
+			assert.ok(oneRecord);
+
+			mongo.getVersions("myCollection", oneRecord._id, function(error, response) {
+				assert.ifError(error);
+				assert.ok(response);
+				console.log(response);
+				assert.equal(response.length, 2);
+				done();
+			});
+		});
+	});
+
+	it("clear record versions", function(done) {
+		mongo.findOne('myCollection', {'a': 'c'}, function(error, oneRecord) {
+			assert.ifError(error);
+			assert.ok(oneRecord);
+
+			mongo.clearVersions("myCollection", oneRecord._id, function(error, response) {
+				assert.ifError(error);
+				assert.ok(response);
+				done();
+			});
+		});
+	});
+
+	it("insert multi records", function(done) {
+		var multiRecords = [
+			{'var1': 'val1'},
+			{'var3': 'val2'},
+			{'var2': 'val3'}
+		];
+
+		mongo.remove('myCollection', {}, function(error) {
+			assert.ifError(error);
+			mongo.insert("myCollection", multiRecords, true, function(error, response) {
+				assert.ifError(error);
+				assert.ok(response);
+
+				mongo.find('myCollection', {}, function(error, records) {
+					assert.ifError(error);
+					assert.ok(records);
+					records.forEach(function(oneRecord) {
+						assert.equal(oneRecord.v, 1);
+						assert.ok(oneRecord.ts);
+					});
+					done();
+				});
+			});
+		});
+	});
 });
