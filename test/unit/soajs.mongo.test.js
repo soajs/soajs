f9b1c447c7f76651fc093869a097ef21e2ae595f
mikehajj <mike.hajj@gmail.com>
2015-07-31 12:40:25 +0300

    Added new Test to increase coverage.

--- test/unit/soajs.mongo.test.js
+++ test/unit/soajs.mongo.test.js
@@ -154,6 +154,47 @@ describe("testing connection", function() {
 			done();
 		});
 	});
+
+    it("testing get Mongo Skin DB", function(done) {
+        var dbConfig = {
+            "name": 'core_provision',
+            "prefix": "soajs_test_",
+            "servers": [
+                {
+                    "host": "127.0.0.1",
+                    "port": "27017"
+                }
+            ],
+            "credentials": null,
+            "URLParam": {
+                "connectTimeoutMS": 0,
+                "socketTimeoutMS": 0,
+                "maxPoolSize": 5,
+                "w": 1,
+                "wtimeoutMS": 0,
+                "slaveOk": true
+            },
+            "extraParam": {
+                "db": {
+                    "native_parser": true
+                },
+                "server": {
+                    "auto_reconnect": true
+                }
+            },
+            'store': {},
+            "collection": "sessions",
+            'stringify': false,
+            'expireAfter': 1000 * 60 * 60 * 24 * 14 // 2 weeks
+        };
+
+        mongo = new soajsMongo(dbConfig);
+        mongo.getMongoSkinDB(function(error, db){
+            assert.ifError(error);
+            assert.ok(db);
+            done();
+        });
+    });
 });
 
 describe("TESTING soajs.mongo", function() {
