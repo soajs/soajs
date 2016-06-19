bfd29730193a52eff8263e7e25a549db4bb5fd4a
antoinehage <me@antoinehage.com>
2016-06-19 09:49:45 -0400

    consolidating error codes under core

--- modules/soajs.mongo/config.js
+++ modules/soajs.mongo/config.js
@@ -1,12 +1,5 @@
 'use strict';
 
 module.exports = {
-    'errors': {
-        '190': 'Unable to build needed url for mongo to connect.',
-        '191': 'collection name is required.',
-        "192": "Invalid record provided to Mongo",
-        "193": "Unable to find the record to version it",
-        "194": "Invalid record version provided to Mongo",
-        "195": "Invalid DB Config. please send a valid DB config to MongoDriver's constructor"
-    }
+
 };
