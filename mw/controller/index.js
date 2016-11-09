63bc4789005fd51a5e6d7d07842e2e8fd411c085
antoinehage <me@antoinehage.com>
2016-11-09 12:19:16 -0500

    cleaned up controller

--- mw/controller/index.js
+++ mw/controller/index.js
@@ -148,58 +148,9 @@ function simpleRTS(req, res) {
             serverResponse.pause();
             serverResponse.headers['access-control-allow-origin'] = '*';
 
-            switch (serverResponse.statusCode) {
-                case 200:
-                case 201:
-                case 202:
-                case 203:
-                case 204:
-                case 205:
-                case 206:
-                case 304:
-                case 400:
-                case 401:
-                case 402:
-                case 403:
-                case 404:
-                case 405:
-                case 406:
-                case 407:
-                case 408:
-                case 409:
-                case 410:
-                case 411:
-                case 412:
-                case 413:
-                case 414:
-                case 415:
-                case 416:
-                case 417:
-                case 418:
-                    res.writeHeader(serverResponse.statusCode, serverResponse.headers);
-                    serverResponse.pipe(res, {end: true});
-                    serverResponse.resume();
-                    break;
-
-                case 301:
-                case 302:
-                case 303:
-                    //serverResponse.statusCode = 303;
-                    res.writeHeader(serverResponse.statusCode, serverResponse.headers);
-                    serverResponse.pipe(res, {end: true});
-                    serverResponse.resume();
-                    break;
-
-                // error everything else
-                default:
-                    res.writeHeader(serverResponse.statusCode, serverResponse.headers);
-                    serverResponse.pipe(res, {end: true});
-                    serverResponse.resume();
-                    break;
-                    //serverResponse.resume();
-                    //req.soajs.controllerResponse(core.error.getError(135));
-                    //break;
-            }
+            res.writeHeader(serverResponse.statusCode, serverResponse.headers);
+            serverResponse.pipe(res, {end: true});
+            serverResponse.resume();
         });
         connector.on('aborted', function (err) {
             req.soajs.log.error(err);
