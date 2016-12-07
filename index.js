fc2cf40ad4e1da975451dccfab7eb9b594e173a0
antoinehage <me@antoinehage.com>
2016-12-07 12:14:38 -0500

    added timestamp to error stack

--- index.js
+++ index.js
@@ -2,23 +2,28 @@
 
 require("./classes/http");
 
+process.on('uncaughtException', function (e) {
+    console.log(new Date().toString(), e.stack || e);
+    process.exit(1);
+});
+
 /**
  *
  * @type {{mw: {soajs: exports, response: exports, rm: exports, inputmask: exports}, restServer: (service|exports), rmServer: (controller|exports)}}
  */
 module.exports = {
-	"server": {
-		"service": require("./servers/service.js"),
-		"controller": require("./servers/controller.js"),
+    "server": {
+        "service": require("./servers/service.js"),
+        "controller": require("./servers/controller.js"),
         "daemon": require("./servers/daemon.js")
-	},
-	"mail": require("./modules/soajs.mail"),
-	"mongo": require("./modules/soajs.mongo"),
-	"es": require("./modules/soajs.es"),
+    },
+    "mail": require("./modules/soajs.mail"),
+    "mongo": require("./modules/soajs.mongo"),
+    "es": require("./modules/soajs.es"),
     "contentBuilder": require("./modules/soajs.contentBuilder"),
-	"hasher": require("./modules/soajs.core").security.hasher,
-	"authorization": {
-		"generate": require("./modules/soajs.core").security.authorization.generate
-	}
+    "hasher": require("./modules/soajs.core").security.hasher,
+    "authorization": {
+        "generate": require("./modules/soajs.core").security.authorization.generate
+    }
 };
 
