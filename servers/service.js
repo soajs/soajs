006b9fc2c51f287e6450e8d5bccd2c4569c3e4de
Etienne <etienne@corsaircms.com>
2017-10-25 17:18:16 +0300

    accommodating swagger with express 4.16.0 updates

--- servers/service.js
+++ servers/service.js
@@ -205,12 +205,16 @@ service.prototype.init = function (callback) {
             soajs.serviceConf = lib.registry.getServiceConf(soajs.param.serviceName, registry);
 
             _self.log = core.getLogger(soajs.param.serviceName, registry.serviceConfig.logger);
-
-            //turn on swagger path
-            if (fs.existsSync('./' + soajs.param.swaggerFilename)) {
-                _self.app.use('/swagger', express.static('./' + soajs.param.swaggerFilename));
-                _self.log.info("Swagger route [/swagger] is ON.");
-            }
+	
+	        //turn on swagger path
+	        if (fs.existsSync('./' + soajs.param.swaggerFilename)) {
+		        _self.app.get('/swagger', function (req, res) {
+			        let swaggerFile = fs.readFileSync('./' + soajs.param.swaggerFilename, {'encoding': 'utf8'});
+			        res.setHeader('Content-Type', 'text/yaml');
+			        res.send(swaggerFile);
+		        });
+		        _self.log.info("Swagger route [/swagger] is ON.");
+	        }
 
             if (process.env.SOAJS_SOLO && process.env.SOAJS_SOLO === "true")
                 _self.log.info("SOAJS is in SOLO mode, the following got turned OFF [extKeyRequired, session, oauth, awareness, awarenessEnv].");
