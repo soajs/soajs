5ef62a3c77b531bae3ab3ca5cf5dcb07a64e9235
antoinehage <me@antoinehage.com>
2019-08-27 14:12:30 -0400

    added placeholder for mt/lib unit test

---
+++ test/unit/mw/mt/lib.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const helper = require("../../../helper.js");
+const lib = helper.requireModule('./mw/mt/lib');
+const assert = require('assert');
+
+
+describe("Unit test for: mw - mt lib", () => {
+
+    it("Add test here", (done) => {
+        done();
+    });
+});
