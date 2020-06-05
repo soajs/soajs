1f2c63efa25d9bd69f3e349dc307830b9b89d848
antoinehage <me@antoinehage.com>
2020-06-05 13:34:22 -0400

    optimized how we handle awareness gethost and connect

--- mw/service/index.js
+++ mw/service/index.js
@@ -99,9 +99,6 @@ module.exports = function (configuration) {
 			req.soajs.awareness = {};
 			req.soajs.awareness.getHost = function () {
 				let host = null;
-				if (!input.awareness || !input.awareness.host) {
-					return host;
-				}
 				let serviceName = null, version = null, cb = arguments[arguments.length - 1];
 				switch (arguments.length) {
 					//controller, cb
@@ -121,6 +118,9 @@ module.exports = function (configuration) {
 						version = arguments[1];
 						break;
 				}
+				if (!input.awareness || !input.awareness.host) {
+					return cb(host);
+				}
 				host = input.awareness.host;
 				let gatewayServiceName = "controller";
 				if (req.soajs && req.soajs.registry && req.soajs.registry.services && req.soajs.registry.services.controller && req.soajs.registry.services.controller.name) {
@@ -138,10 +138,6 @@ module.exports = function (configuration) {
 				return cb(host);
 			};
 			req.soajs.awareness.connect = function () {
-				let response = null;
-				if (!input.awareness || !input.awareness.host) {
-					return response;
-				}
 				let serviceName = null, version = null, cb = arguments[arguments.length - 1];
 				switch (arguments.length) {
 					//controller, cb
@@ -155,7 +151,10 @@ module.exports = function (configuration) {
 						version = arguments[1];
 						break;
 				}
-				response = {};
+				let response = {};
+				if (!input.awareness || !input.awareness.host) {
+					return cb(response);
+				}
 				if (serviceName && param.interConnect && input.awareness.interConnect && Array.isArray(input.awareness.interConnect) && input.awareness.interConnect.length > 0) {
 					for (let i = 0; i < input.awareness.interConnect.length; i++) {
 						let serviceObj = input.awareness.interConnect[i];
