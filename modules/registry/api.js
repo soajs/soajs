b40544a839815cbfbd6b3eec359a223f440ad181
antoinehage <me@antoinehage.com>
2025-03-08 05:59:14 -0500

    removed dependency from request package

--- modules/registry/api.js
+++ modules/registry/api.js
@@ -8,7 +8,8 @@
  * found in the LICENSE file at the root of this repository
  */
 
-const request = require('request');
+// const request = require('request');
+const { httpRequestLight } = require("../../utilities/request.js");
 
 if (!process.env.SOAJS_REGISTRY_API || process.env.SOAJS_REGISTRY_API.indexOf(":") === -1) {
 	throw new Error('Invalid format for SOAJS_REGISTRY_API [hostname:port]: ' + process.env.SOAJS_REGISTRY_API);
@@ -23,19 +24,30 @@ if (!process.env.SOAJS_REGISTRY_API || process.env.SOAJS_REGISTRY_API.indexOf(":
 let model = {
 	"fetchRegistry": function (param, options, cb) {
 		let reg = null;
-		
+
 		let requestOption = {
-			"url": "http://" + process.env.SOAJS_REGISTRY_API + "/getRegistry?env=" + options.envCode + "&name=" + param.name + "&type=" + param.type + "&setBy=" + options.setBy,
+			"uri": "http://" + process.env.SOAJS_REGISTRY_API + "/getRegistry?env=" + options.envCode + "&name=" + param.name + "&type=" + param.type + "&setBy=" + options.setBy,
 			"json": true
 		};
-		request(requestOption, function (error, response, body) {
-			if (!error) {
+		httpRequestLight(requestOption)
+			.then((body) => {
 				if (body && body.result) {
 					reg = body.data;
 				}
-			}
-			return cb(error, reg);
-		});
+				return cb(null, reg);
+			})
+			.catch((error) => {
+				return cb(error, reg);
+			});
+
+		// request(requestOption, function (error, response, body) {
+		// 	if (!error) {
+		// 		if (body && body.result) {
+		// 			reg = body.data;
+		// 		}
+		// 	}
+		// 	return cb(error, reg);
+		// });
 	}
 };
 
