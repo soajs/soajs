296d4005bf692c12f07ceea2fb860c7b67e29d3f
antoinehage <me@antoinehage.com>
2025-11-11 13:00:29 -0500

    removed request

--- test/integration/requester.js
+++ test/integration/requester.js
@@ -9,23 +9,40 @@
 "use strict";
 
 const assert = require('assert');
-let request = require("request");
+const axios = require("axios");
 
 let extKey = '3d90163cf9d6b3076ad26aa5ed58556348069258e5c6c941ee0f18448b570ad1c5c790e2d2a1989680c55f4904e2005ff5f8e71606e4aa641e67882f4210ebbc5460ff305dcb36e6ec2a2299cf0448ef60b9e38f41950ec251c1cf41f05f3ce9';
 let accessToken = "44a5399dcce96325fadfab908e614bf00e6fe967";
 
-module.exports = (method, params, cb) => {
+module.exports = async (method, params, cb) => {
+	// Map 'del' to 'delete' for proper HTTP method
+	const httpMethod = (method === 'del') ? 'delete' : method;
+
 	let options = {
-		uri: params.uri,
+		url: params.uri,
+		method: httpMethod.toUpperCase(),
 		headers: {
-			key: extKey
+			key: extKey,
+			'Accept': 'application/json'
 		},
-		method: method.toUpperCase(),
-		json: true
+		responseType: 'json',  // Explicitly request JSON responses
+		transformResponse: [(data) => {
+			// Handle JSON parsing explicitly
+			if (typeof data === 'string') {
+				try {
+					return JSON.parse(data);
+				} catch (e) {
+					return data;
+				}
+			}
+			return data;
+		}]
 	};
+
 	if (!params.public){
 		options.headers.access_token = accessToken;
 	}
+
 	if (params.headers) {
 		for (let header in params.headers) {
 			if (Object.hasOwnProperty.call(params.headers, header)) {
@@ -33,26 +50,38 @@ module.exports = (method, params, cb) => {
 			}
 		}
 	}
+
+	// Handle form data
 	if (params.form) {
-		options.form = params.form;
+		options.data = params.form;
+		options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
 	}
+
+	// Handle JSON body
 	if (params.body) {
-		options.body = params.body;
+		options.data = params.body;
 	}
+
+	// Handle query string parameters
 	if (params.qs) {
-		options.qs = params.qs;
+		options.params = params.qs;
 	}
-	if (method === 'delete') {
-		request.del(options, function (error, response, body) {
-			assert.ifError(error);
-			assert.ok(body);
+
+	try {
+		const response = await axios(options);
+		// Don't assert on response.data - some responses (like DELETE) may have empty bodies
+		return cb(null, response.data);
+	} catch (error) {
+		// axios throws on non-2xx responses, but integration tests expect callback with data
+		if (error.response) {
+			// HTTP error response (any status code outside 2xx)
+			// Integration tests expect these responses to be passed through, not treated as errors
+			const body = error.response.data;
 			return cb(null, body);
-		});
-	} else {
-		request[method](options, function (error, response, body) {
+		} else {
+			// Network or other error (no response received)
 			assert.ifError(error);
-			assert.ok(body);
-			return cb(null, body);
-		});
+			return cb(error);
+		}
 	}
 };
